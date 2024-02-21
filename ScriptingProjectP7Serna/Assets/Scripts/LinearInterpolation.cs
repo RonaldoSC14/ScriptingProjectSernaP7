aed59964e844b8a296e8b98fd4a5f42047e1d40b
Salvador Parajon <rcserna09@gmail.com>
2024-02-21 12:41:33 -0800

    LinearInterpolations
    Added a code in the Linear interpolation script.

--- ScriptingProjectP7Serna/Assets/Scripts/LinearInterpolation.cs
+++ ScriptingProjectP7Serna/Assets/Scripts/LinearInterpolation.cs
@@ -1,17 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class LinearInterpolation : MonoBehaviour
 {
     // In this case, result = 8
     float result = Mathf.Lerp(6f, 10f, 0.5f);
 
+    Vector3 from = Vector3.zero;
+    Vector3 to = Vector3.zero;
+
     // Update is called once per frame
     void Update()
     {
-      
+        Component = Mathf.Lerp(Component.intensity, 8f, 0.5f);
     }
 
-    
+    private void LateUpdate()
+    {
+        Component.intensit = Mathf.Lerp(Component.intensity, 8f, 0.5f * Time.deltaTime);
+    }
+
+
 }
