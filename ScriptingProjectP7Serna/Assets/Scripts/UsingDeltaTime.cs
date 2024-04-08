72e96018890dc9bb11f77a89a31eaf2e54d28e86
Salvador Parajon <rcserna09@gmail.com>
2024-04-08 12:27:38 -0700

    Delta Time
    Added the delta time scripts.

---
+++ ScriptingProjectP7Serna/Assets/Scripts/UsingDeltaTime.cs
@@ -0,0 +1,23 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UsingDeltaTime : MonoBehaviour
+{
+    public float speed = 8f;
+    public float countdown = 3.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        if (countdown <= 0.0f)
+            GetComponent<Light>().enabled = true;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.position += new Vector3(speed * Time.deltaTime, 0.0f, 0.0f);
+    }
+}
